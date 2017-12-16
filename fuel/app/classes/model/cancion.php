78cb7f29b221321b9787250935d06dd38f983fae
Jeixam <jaime_rodriguez_apps1t1617@gmail.com>
2017-12-16 18:28:59 -0500

    first commit

--- fuel/app/classes/model/cancion.php
+++ fuel/app/classes/model/cancion.php
@@ -2,7 +2,7 @@
 
 class Model_Users extends Orm\Model
 {
-    protected static $_table_name = 'cancion';
+    protected static $_table_name = 'listas';
     protected static $_primary_key = array('id');
     protected static $_properties = array(
         'id', // both validation & typing observers will ignore the PK
@@ -17,7 +17,6 @@ class Model_Users extends Orm\Model
         'url' => array
         (
             'data_type' => 'varchar'
-<<<<<<< HEAD
         ),
         'id_usuarios' => array
         (
@@ -25,7 +24,7 @@ class Model_Users extends Orm\Model
         )
         );
     protected static $_many_many = array(
-    'cancion' => array(
+    'listas' => array(
         'key_from' => 'id',
         'key_through_from' => 'id_listas', // column 1 from the table in between, should match a posts.id
         'table_through' => 'tienen', // both models plural without prefix in alphabetical order
@@ -36,8 +35,6 @@ class Model_Users extends Orm\Model
         'cascade_delete' => false,
     )
 );
-=======
         )
         );
->>>>>>> 9f5cdbe7b7d24a0d74b27e102816c48956357333
 }
--- fuel/app/classes/model/listas.php
+++ fuel/app/classes/model/listas.php
@@ -15,12 +15,11 @@ class Model_Listas extends Orm\Model
             'data_type' => 'int'   
         )
         );
-<<<<<<< HEAD
 
 protected static $_many_many = array(
     'cancion' => array(
         'key_from' => 'id',
-        'key_through_from' => 'id_cancion', // column 1 from the table in between, should match a posts.id
+        'key_through_from' => 'id_canciones', // column 1 from the table in between, should match a posts.id
         'table_through' => 'tienen', // both models plural without prefix in alphabetical order
         'key_through_to' => 'id_listas', // column 2 from the table in between, should match a users.id
         'model_to' => 'Model_cancion',
@@ -32,11 +31,6 @@ protected static $_many_many = array(
 protected static $_belongs_to = array(
     'usuarios' => array(
         'key_from' => 'id_usuario',
-=======
-protected static $_belongs_to = array(
-    'usuarios' => array(
-        'key_from' => 'usuario_id',
->>>>>>> 9f5cdbe7b7d24a0d74b27e102816c48956357333
         'model_to' => 'Model_users',
         'key_to' => 'id',
         'cascade_save' => true,
